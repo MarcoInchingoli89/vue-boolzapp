254c290271ca28d4a42d5eec0018802ab29bc11f
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-02 19:17:45 +0100

    Add Contacts MIlestone 1 Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ createApp({
       contacts: [
         {
           name: 'Michele',
-          avatar: '_1',
+          avatar: './assets/img/avatar_1.jpg',
           visible: true,
           messages: [
             {
@@ -38,7 +38,7 @@ createApp({
         },
         {
           name: 'Fabio',
-          avatar: '_2',
+          avatar: './assets/img/avatar_2.jpg',
           visible: true,
           messages: [
             {
@@ -60,7 +60,7 @@ createApp({
         },
         {
           name: 'Samuele',
-          avatar: '_3',
+          avatar: './assets/img/avatar_3.jpg',
           visible: true,
           messages: [
             {
@@ -82,7 +82,7 @@ createApp({
         },
         {
           name: 'Alessandro B.',
-          avatar: '_4',
+          avatar: './assets/img/avatar_4.jpg',
           visible: true,
           messages: [
             {
@@ -99,7 +99,7 @@ createApp({
         },
         {
           name: 'Alessandro L.',
-          avatar: '_5',
+          avatar: './assets/img/avatar_5.jpg',
           visible: true,
           messages: [
             {
@@ -116,7 +116,7 @@ createApp({
         },
         {
           name: 'Claudia',
-          avatar: '_6',
+          avatar: './assets/img/avatar_6.jpg',
           visible: true,
           messages: [
             {
@@ -138,7 +138,7 @@ createApp({
         },
         {
           name: 'Federico',
-          avatar: '_7',
+          avatar: './assets/img/avatar_7.jpg',
           visible: true,
           messages: [
             {
@@ -155,7 +155,7 @@ createApp({
         },
         {
           name: 'Davide',
-          avatar: '_8',
+          avatar: './assets/img/avatar_8.jpg',
           visible: true,
           messages: [
             {
